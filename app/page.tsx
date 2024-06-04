6508337dbf111f4103ef4b56d66c3da77d885448
Subhajit Kundu <subha60kundu@gmail.com>
2024-06-04 09:03:44 +0530

    page: Creating the home page

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,29 @@
-import Counter from '@/components/Counter'
+import stopwatch from '@/public/stopwatch.svg'
+import Image from 'next/image'
+import Button from '@/components/Button'
 
 export default function Home() {
 	const initialTime = Date.now()
 
 	return (
-		<main className="flex min-h-svh flex-col items-center justify-center p-24 gap-8">
-			<Counter initialTime={initialTime} />
+		<main className="flex min-h-svh w-screen mx-auto max-w-[700px] flex-col items-start justify-end p-8 gap-8">
+			<h1 className="flex flex-col text-5xl leading-[1.15] lg:flex-row lg:flex-wrap">
+				<span className="text-yellow-400 whitespace-nowrap">Measure</span>
+				<span><span className="italic whitespace-nowrap">time</span> <span className="underline decoration-yellow-400 whitespace-nowrap">of your</span></span>
+				<span className="font-semibold whitespace-nowrap">life events</span>
+			</h1>
+
+			<p className="text-sm opacity-60 max-w-96 text-balance">
+				Measure the duration of any situation, event, occurrences like any event that goes on for multiple
+				hours or days on end
+			</p>
+
+			<Image src={stopwatch} alt="Stopwatch image used for illustrative purposes" className="mt-8 mx-auto max-w-48" />
+
+			<div className="space-y-2 w-full lg:flex lg:items-center lg:gap-2 lg:space-y-0">
+				<Button size="full">Get started</Button>
+				<Button size="full" variant="secondary">About Ticker and the creator</Button>
+			</div>
 		</main>
 	)
 }
---
+++ components/Button.tsx
@@ -0,0 +1,48 @@
+import {ReactNode} from 'react'
+import {cva} from 'class-variance-authority'
+
+const button = cva('rounded-2xl transition', {
+	variants: {
+		variant: {
+			primary: [
+				'bg-yellow-400',
+				'text-neutral-950',
+				'border border-transparent',
+				'font-medium',
+				'hover:bg-yellow-500',
+			],
+			secondary: [
+				'bg-transparent',
+				'text-yellow-400',
+				'border border-yellow-400',
+				'hover:bg-yellow-400/10',
+			],
+		},
+		size: {
+			small: ['text-sm', 'py-1', 'px-2'],
+			base: ['text-base', 'py-2', 'px-4'],
+			full: ['w-full', 'py-3', 'px-4']
+		},
+	},
+	defaultVariants: {
+		variant: 'primary',
+		size: 'base',
+	},
+})
+
+interface ButtonProps {
+	children: ReactNode
+	variant?: 'primary' | 'secondary'
+	size?: 'small' | 'base' | 'full'
+	className?: string
+}
+
+const Button = ({className, variant, size, children, ...props }: ButtonProps) => {
+	return (
+		<button className={button({ variant, size, className })} {...props}>
+			{children}
+		</button>
+	)
+}
+
+export default Button
