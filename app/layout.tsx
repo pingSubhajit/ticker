304fef70b756337f3d10671790403ca4298beb00
Subhajit Kundu <subha60kundu@gmail.com>
2024-06-05 23:28:52 +0530

    chore: Removing newline from meta description

--- app/layout.tsx
+++ app/layout.tsx
@@ -10,7 +10,7 @@ export const metadata: Metadata = {
 	metadataBase: new URL(defaultUrl),
 	title: 'Ticker - Long Duration Stopwatch',
 	description: 'Ticker is a mobile-first, long-duration, cloud-synced stopwatch application. Track time for days, ' +
-		'months, or even years. Create and access your stopwatches from anywhere. Free and open-source with an MIT license.\n',
+		'months, or even years. Create and access your stopwatches from anywhere. Free and open-source with an MIT license.',
 	keywords: ['stopwatch', 'timer', 'long duration', 'cloud synced', 'open source', 'mobile first', 'minimalistic',
 		'Ticker', 'long duration stopwatch', 'cloud synced stopwatch', 'open source stopwatch', 'minimalistic stopwatch',
 		'mobile stopwatch app', 'free stopwatch', 'stopwatch for days', 'stopwatch for months', 'stopwatch for years']
