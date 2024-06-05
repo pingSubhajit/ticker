03554d882c4a65a7c3a43f7c506431a2a50d5f5a
Subhajit Kundu <subha60kundu@gmail.com>
2024-06-05 23:56:34 +0530

    changing getCurrentTimestamp function to simple Date.now()

--- lib/utils.ts
+++ lib/utils.ts
@@ -28,10 +28,11 @@ export type Breakdown = {
 }
 
 export const getCurrentPSTUnixTimestamp = () => {
-	const date = new Date()
-	const timezoneOffset = date.getTimezoneOffset()
-	const pstOffset = 350 // this is the offset for the Pacific Standard Time timezone
-	// Get UNIX timestamp in milliseconds and adjust for the timezone offset
-	const adjustedTime = new Date(date.getTime() + (timezoneOffset + pstOffset) * 60000)
-	return adjustedTime.getTime()
+	// const date = new Date()
+	// const timezoneOffset = date.getTimezoneOffset()
+	// const pstOffset = 350 // this is the offset for the Pacific Standard Time timezone
+	// // Get UNIX timestamp in milliseconds and adjust for the timezone offset
+	// const adjustedTime = new Date(date.getTime() + (timezoneOffset + pstOffset) * 60000)
+	// return adjustedTime.getTime()
+	return Date.now()
 }
