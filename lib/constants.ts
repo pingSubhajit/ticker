9379f33ff10dfff65a5f2bd6e2d2260e02d38991
Subhajit Kundu <subha60kundu@gmail.com>
2024-06-07 21:01:36 +0530

    docs: Removed arbitrary local ip value in the default URL field in constants.ts file and implemented new optional env variable

--- lib/constants.ts
+++ lib/constants.ts
@@ -1,3 +1,3 @@
-export const defaultUrl = process.env.VERCEL_URL ? 'https://tickernow.xyz' : 'http://192.168.0.109:3000'
+export const defaultUrl = process.env.VERCEL_URL ? 'https://tickernow.xyz' : process.env.MAIN_URL || 'http://localhost:3000'
 export const githubRepo = 'https://github.com/pingSubhajit/ticker'
 export const portfolio = 'https://subhajitkundu.me'
